caa7f26a60e259e087379947fd0399d4bb0812dc
tobias.moullet <tobias.moullet@hefr.ch>
2025-07-30 16:05:04 +0200

    fix doc deploy

--- include/bluesync/bluesync.h
+++ include/bluesync/bluesync.h
@@ -13,25 +13,39 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- * File: bluesync.h
- * Description: Public API for the BlueSync time synchronization module.
- *
  * Project: BlueSync - BLE Time Sync for Zephyr
  * Repository: https://github.com/Tobi15/zephyr-bluesync-ble
  */
 
+/**
+ * @file bluesync.h
+ * @brief Public API for the BlueSync time synchronization module.
+ *
+ * This file defines the core API functions for initializing and managing
+ * time synchronization using the BlueSync protocol in a BLE Mesh environment.
+ */
+
+/**
+ * @defgroup bluesync_api BlueSync Public API
+ * @brief Functions and definitions to use the BlueSync time synchronization system.
+ *
+ * This group contains the public interface of the BlueSync module.
+ * @{
+ */
 
 #ifndef BLUESYNC_H
 #define BLUESYNC_H
 
 #include <zephyr/kernel.h>
 
+/**
+ * @brief Defines the operational roles for a BlueSync node.
+ */
 typedef enum {
-	BLUESYNC_NONE_ROLE = 0,
-	BLUESYNC_AUTHORITY_ROLE = 1,
-	BLUESYNC_CLIENT_ROLE = 2
+	BLUESYNC_NONE_ROLE = 0,        /**< No role assigned. */
+	BLUESYNC_AUTHORITY_ROLE = 1,   /**< Acts as the time authority (reference clock). */
+	BLUESYNC_CLIENT_ROLE = 2       /**< Acts as a time-synchronized client. */
 } bluesync_role_t;
-
 
 /**
  * @brief Initializes the BlueSync time synchronization module.
@@ -40,58 +54,50 @@ typedef enum {
  * variables required for BlueSync to operate. It should be called once
  * at startup before any synchronization or logical time queries are made.
  */
-void bluesync_init();
+void bluesync_init(void);
 
 /**
  * @brief Sets the operational role of the BlueSync node.
  *
  * This function configures the role of the device within the BlueSync
  * synchronization system. Roles define whether the device acts as a time authority
- * or as a time client within the network.
+ * or as a client.
  *
- * A **time authority** is the unique source of reference time and distributes it
- * throughout the network. A **time client** receives synchronization from the authority
- * or other clients and can forward the synchronized time further once it is synchronized.
- *
- * @param role The role to assign to the device. Must be one of the values defined in
- *             the @ref bluesync_role_t enumeration:
+ * @param role Role to assign to the device.
+ *             Must be one of @ref bluesync_role_t:
  *             - BLUESYNC_AUTHORITY_ROLE
  *             - BLUESYNC_CLIENT_ROLE
  */
 void bluesync_set_role(bluesync_role_t role);
 
 /**
- * @brief Initiates a new BlueSync synchronization round as the time authority.
+ * @brief Starts a BlueSync synchronization round as the time authority.
  *
- * This function is called by the node acting as the time authority to start
- * a network-wide synchronization round. It broadcasts timing information to
- * all clients, allowing them to synchronize their local clocks based on the
- * authority's reference time.
- *
- * This function should only be called by devices assigned the
- * @ref BLUESYNC_AUTHORITY_ROLE.
+ * This should be called by the device with the BLUESYNC_AUTHORITY_ROLE to
+ * broadcast synchronization data to clients.
  */
-void bluesync_start_net_sync();
+void bluesync_start_net_sync(void);
 
 /**
- * @brief Initiates a new BlueSync synchronization round as the time authority.
+ * @brief Starts a synchronization round using a known UNIX epoch time.
  *
- * This function is called by the node acting as the time authority to start
- * a network-wide synchronization round. It broadcasts timing information to
- * all clients, allowing them to synchronize their local clocks based on the
- * authority's reference time.
+ * This function is used by the authority node to synchronize the network based on
+ * an external UNIX time reference.
  *
- * This function should only be called by devices assigned the
- * @ref BLUESYNC_AUTHORITY_ROLE.
- * @param unix_epoch_ms : unix epoch reference in us
+ * @param unix_epoch_us UNIX epoch timestamp in microseconds.
  */
 void bluesync_start_net_sync_with_unix_epoch_us(uint64_t unix_epoch_us);
 
 /**
- * @brief Get the current time which is based on the UNIX reference. 
- * 
- * @return return unix timestamp in microsecond in uint64_t
+ * @brief Gets the current synchronized UNIX time.
+ *
+ * This returns the current time as synchronized with the authority node,
+ * expressed in microseconds since the UNIX epoch.
+ *
+ * @return Current synchronized UNIX timestamp in microseconds.
  */
 uint64_t get_current_unix_time_us(void);
 
 #endif /* BLUESYNC_H */
+
+/** @} */ // end of bluesync_api
