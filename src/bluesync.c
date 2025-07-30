77b5e5d91ea1423398bcf399ed8b30c9488cbaa5
tobias.moullet <tobias.moullet@hefr.ch>
2025-07-30 17:31:33 +0200

    modify a header

--- src/bluesync.c
+++ src/bluesync.c
@@ -1,14 +1,24 @@
-/*********************************************************************************
- * @copyright (c) 2024 University of Applied Sciences and Arts Western Switzerland
- * All rights reserved.
+/*
+ * Copyright 2025 Tobias Moullet
  *
- * Unauthorized copying of this file, via any medium is strictly prohibited.
- * Proprietary and confidential
- *********************************************************************************
- * Project : HEIA-FR / Autonomous Metrology in Underground Sites
- * @file   : bluesync.c
- * @date   : 10.12.2024
- ********************************************************************************/
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * File: bluesync.c
+ * Description: Private API for the BlueSync time synchronization module.
+ *
+ * Project: BlueSync - BLE Time Sync for Zephyr
+ * Repository: https://github.com/Tobi15/zephyr-bluesync-ble
+ */
 
 #include <zephyr/kernel.h>
 #include <zephyr/bluetooth/bluetooth.h>
